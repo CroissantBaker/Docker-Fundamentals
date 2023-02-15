3c5625fc7be1773d10c90145e7345045acdd82a4
CroissantBaker <sync_canada@protonmail.com>
2023-02-14 20:52:46 -0500

    nothing

--- src/hello-world.py
+++ src/hello-world.py
@@ -2,7 +2,6 @@ import sys
 
 # if we pass an argument we print it, if not we just print hello world
 if len(sys.argv) > 1:
-    print(f"hello-world {sys.argv[1]}")
+    print(f"hello-world !!! {sys.argv[1]}")
 else:
     print(f"hello-world")
-
